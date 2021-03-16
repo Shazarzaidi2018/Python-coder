fcbbc951cfd683bbbd50135a17d651110cd26065
Shazarzaidi2018 <74261955+Shazarzaidi2018@users.noreply.github.com>
2021-03-16 14:00:19 +0530

    About calculater
    You can calculate any two numbers.

---
+++ hw.py
@@ -0,0 +1,20 @@
+a=int(input('enter 1st number:'))
+b=int(input('enter 2nd number:'))
+d=input('enter a choise one operator:')
+if(d=='+'):
+       c=a+b
+       print(c)
+elif(d=='-'):
+       e=a-b
+       print(e)
+elif(d=='*'):
+       f=a*b
+       print(f)
+elif(d=='%'):
+       g=a%b
+       print(g)
+elif(d=='/'):
+       h=a/b
+       print(h)
+else:
+       print("no")
